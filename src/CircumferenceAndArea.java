ca034d9ecba64dc2edddf2751ee5cf541d339535
ecemtellal <91099163+ecemtellal@users.noreply.github.com>
2024-04-03 22:38:00 +0300

    First Commit

--- src/CircumferenceAndArea.java
+++ src/CircumferenceAndArea.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class CircumferenceAndArea {
+import java.util.Scanner;
+public class CircumferenceAndArea {
+    public static void main(String[] args) {
+        int radius ,centralAngle;
+        double area,circumference,circleSliceArea, pi=3.14;
+
+        Scanner input=new Scanner (System.in);
+        System.out.println("Dairenin yarıçapını giriniz: ");
+        radius=input.nextInt();
+        System.out.println("Dairenin merkez açısını giriniz: ");
+        centralAngle=input.nextInt();
+
+        circumference= 2 * pi * radius;
+        area=pi *radius * radius;
+        circleSliceArea= (pi*(radius*radius)*centralAngle)/360 ;
+
+        System.out.println("Dairenin çevresi: " + circumference);
+        System.out.println("Dairenin Alanı: " + area);
+        System.out.println("Daire Diliminin Alanı :" + circleSliceArea);
+
+    }
 }
